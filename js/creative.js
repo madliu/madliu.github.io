270b645c55403b4b66150cff6ebd2b0ab8a4dad8
Madli Kivisik <madli@pactum.com>
2020-10-05 11:24:16 +0300

    dashboard online

--- js/creative.js
+++ js/creative.js
@@ -46,3 +46,13 @@
     new WOW().init();
 
 })(jQuery); // End of use strict
+
+
+$("button").click(function() {
+    var show = $(this).attr('class');
+    $('.post').each(function(){
+        $(this).show();
+        var test = $(this).attr('class');
+        if (test.indexOf(show) < 0) $(this).hide();
+    });
+});
